b6155902c4330920af657f925d4e0c7861a5fff4
Ruan Scherer <ruan.vscherer@gmail.com>
2020-07-22 18:27:04 -0300

    Address register added

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -3,3 +3,33 @@ function logout() {
 	localStorage.removeItem("accountType")
 	window.location.replace("../index.html")
 }
+
+function getUrlParams() {
+	const { href } = window.location
+	let params = href.slice(href.indexOf("?") + 1, href.length)
+	params = params.split("&")
+	params = params.map(param => {
+		parts = param.split("=")
+		return { key: parts[0], value: parts[1] }
+	})
+	return params
+}
+
+function getUrlParam(name) {
+	const params = getUrlParams()
+	return params.filter(param => param.key === name)[0].value
+}
+
+function getTokenData() {
+	let tokenData = localStorage.getItem("accessToken").split(".")[1]
+	return JSON.parse(atob(tokenData)) 
+}
+
+function authValidation(accountType) {
+	if (!localStorage.getItem("accessToken") || !localStorage.getItem("accountType")) {
+		if (localStorage.getItem("accountType") != accountType) {
+			window.location.href = "../entrar.html"
+			return
+		}
+	}
+}
